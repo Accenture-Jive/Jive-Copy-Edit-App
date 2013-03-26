9aeba60b0992ae0ee10b401af004e1b240e07211
Suresh Peddapu <suresh.peddapu@accenture.com>
2013-03-26 12:58:22 +0530

    commit changes

--- js/copyedit.js
+++ js/copyedit.js
@@ -812,7 +812,7 @@ else if(pollIndex!=-1)
 	alert("parent: "+commentData.list[i].resources.self.ref);
     var comment=new osapi.jive.corev3.contents.Comment();
     comment.content=commentData.list[i].content;
-    comment.parent=commentData.list[i].resources.self.ref;
+    //comment.parent=commentData.list[i].resources.self.ref;
 	response.createComment(comment).execute(); 
     }
 	}
