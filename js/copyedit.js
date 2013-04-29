8b2cc059f94d349c9a180416abbcc27b48f5bb72
Ripul Thakur <ripul.thakur@accenture.com>
2013-04-29 12:02:33 +0530

    commit changes

--- js/copyedit.js
+++ js/copyedit.js
@@ -900,7 +900,7 @@ else if(pollIndex!=-1)
 function executeCommentCopy() {
 		alert("Into the execute comment copy :-");
 		alert("commentDataIndex: "+commentDataIndex);
-		alert("commentData legth: "+commentData.length;
+		alert("commentData legth: "+commentData.length);
 		if(commentDataIndex == commentData.length) 
 		{
 			console.log("commentData: "+commentData);
