72228d983ede3f70585ed79917d904cf3085c5fd
Suresh Peddapu <suresh.peddapu@accenture.com>
2013-06-14 20:10:32 +0530

    commit changes

--- js/copyedit.js
+++ js/copyedit.js
@@ -142,7 +142,7 @@ if(response.list[0].resources.hasOwnProperty('messages'))
  messageData=data;
  
  
- var d = messageData.list[commentDataIndex].updated,
+ var d = new Date,
     dformat = [d.getDate(),
                d.getMonth()+1,
                d.getFullYear()].join('/')+
